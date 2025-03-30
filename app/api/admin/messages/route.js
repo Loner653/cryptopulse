30454e13342b084e3d19598b785f329f5cf3a470
Loner653 <perrykate751@gmail.com>
2025-03-30 16:09:09 +0200

    Fixed import paths

--- app/api/admin/messages/route.js
+++ app/api/admin/messages/route.js
@@ -1,5 +1,5 @@
-import { db } from "../../../lib/db";
-import { messages } from "../../../lib/db/schema";
+import { db } from "@/lib/db";
+import { messages } from "@/lib/db/schema";
 
 export async function GET() {
     try {
